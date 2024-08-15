6b28fa7fa2feddc83c48041f9b5d07672e5a722c
Tanzeel Alam <tanzeelalam@gmail.com>
2024-08-15 12:27:11 +0530

    Added call to dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tanzeelalam/puppy
 
 go 1.23.0
+
+require github.com/tanzeelalam/dog v0.0.0-20240815064859-66b95cdc8ade // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/tanzeelalam/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
